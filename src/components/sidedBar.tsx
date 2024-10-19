49d62a68f5f3e0853093085170fa6dcaefa98a47
oHminod <yvesm13@gmail.com>
2024-10-19 18:19:56 +0200

    height responsive sidebar

--- src/components/sidedBar.tsx
+++ src/components/sidedBar.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 const SidedBar = () => {
+  const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight);
+
+  useEffect(() => {
+    function calculateWindowHeight() {
+      setWindowHeight(window.innerHeight);
+    }
+    window.addEventListener("resize", calculateWindowHeight);
+    return () => {
+      window.removeEventListener("resize", calculateWindowHeight);
+    };
+  }, []);
+
+  const isSmallScreen = windowHeight < 768;
+  const isVerySmallScreen = windowHeight < 328;
+
   return (
     <div className="min-h-full w-[117px] bg-[#020203] fixed z-10">
-      <nav className="flex flex-col items-center justify-center gap-5 h-full w-full absolute -top-[6px] z-10">
+      <nav
+        className={`flex flex-col items-center justify-center gap-5 h-full w-full absolute -top-[6px] z-10${
+          isSmallScreen ? " hidden" : ""
+        }
+        `}
+      >
         <a href="#">
           <div className="bg-white h-16 w-16 rounded-[6px] flex items-center justify-center">
             <img src="/Zen.png" alt="zen" />
@@ -23,7 +45,11 @@ const SidedBar = () => {
           </div>
         </a>
       </nav>
-      <p className="fixed text-white text-xs font-medium rotate-[270deg] -left-[14px] bottom-[120px]">
+      <p
+        className={`fixed text-white text-xs font-medium rotate-[270deg] -left-[14px] bottom-[120px]${
+          isVerySmallScreen ? " hidden" : ""
+        }`}
+      >
         Copyright, SportSee 2020
       </p>
     </div>

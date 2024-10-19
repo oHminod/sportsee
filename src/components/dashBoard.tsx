ed7b707a6edfe619b39cbe24958804d5d7853323
oHminod <yvesm13@gmail.com>
2024-10-19 18:25:23 +0200

    responsive

--- src/components/dashBoard.tsx
+++ src/components/dashBoard.tsx
@@ -30,28 +30,28 @@ const DashBoard = (): JSX.Element => {
   const goodScore = user.score >= 0.15;
 
   return (
-    <div className="flex flex-col w-full pl-[117px] pt-[91px] pb-[68px]">
+    <div className="flex flex-col w-full px-0 lg:px-2 lg:pl-[117px] pt-[91px] pb-[68px]">
       <div className="flex flex-col items-center justify-center mb-[77px] mt-[68px]">
-        <h1 className="w-[835px] xl:w-[1125px] text-5xl leading-6 font-medium">
+        <h1 className="lg:w-[835px] xl:w-[1125px] text-5xl leading-6 font-medium">
           Bonjour{" "}
           <span className="text-primary">{user.userInfos.firstName}</span>
         </h1>
-        <p className="w-[835px] xl:w-[1125px] text-lg leading-6 mt-[41px] ml-[2px]">
+        <p className="lg:w-[835px] xl:w-[1125px] text-lg leading-6 mt-[41px] ml-[2px]">
           {goodScore
             ? "Félicitation ! Vous avez explosé vos objectifs hier 👏"
             : "Courage ! Continuez, vos efforts vont payer 💪"}
         </p>
       </div>
-      <div className="flex flex-col items-center xl:flex-row xl:justify-center xl:items-start gap-8">
+      <div className="flex flex-col px-4 lg:px-0 lg:items-center xl:flex-row xl:justify-center xl:items-start gap-8">
         <div className="flex flex-col gap-[30px]">
           <ActivityBarChart userActivity={userActivity} />
-          <div className="flex justify-between w-[835px]">
+          <div className="flex flex-wrap gap-2 justify-center lg:justify-between w-full lg:w-[835px]">
             <SessionLengthLineChart userAverageSessions={userAverageSessions} />
             <PerformanceRadar userPerformance={userPerformance} />
             <ScoreRadialChart user={user} />
           </div>
         </div>
-        <div className="flex xl:flex-col gap-[39px] justify-between w-[835px] xl:w-[258px]">
+        <div className="flex flex-wrap xl:flex-col gap-[39px] justify-center lg:justify-between lg:w-[835px] xl:w-[258px]">
           {keyData.map((data, i) => (
             <UserInfoCard key={i} label={data.key} value={data.value} />
           ))}

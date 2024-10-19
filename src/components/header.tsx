8315e7d01c1a4ea73c68f9f426b81b98b9bcc7d1
oHminod <yvesm13@gmail.com>
2024-10-19 18:22:46 +0200

    responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
     <div className="h-[91px] flex bg-[#020203] items-center shadow-header fixed top-0 left-0 right-0 z-50">
-      <div className="flex w-[210px] ml-[29px]">
+      <div className="w-[178px] ml-[29px]">
         <img src="/logo.png" alt="logo" className="w-full h-[60px]" />
       </div>
-      <nav className="w-full pl-[149px] pr-[91px]">
+      <nav className="flex-1 w-full pl-[149px] pr-[91px] hidden lg:block">
         <ul className="flex gap-6 ml-auto mr-[29px] w-full justify-between items-center">
           <li>
             <a href="#" className="text-white font-medium text-2xl leading-6">

8a53a9c91dd41c7ed7e3040a120ea7dced990c54
oHminod <yvesm13@gmail.com>
2024-10-19 06:04:28 +0200

    merging small app.tsx into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Header from "./components/header.tsx";
+import SidedBar from "./components/sidedBar.tsx";
+import DashBoard from "./components/dashBoard.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Header />
+    <SidedBar />
+    <DashBoard />
+  </StrictMode>
+);

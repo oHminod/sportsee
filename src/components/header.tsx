0e8bba9ce04d0d3e92cb6ad407be93a054947b06
oHminod <yvesm13@gmail.com>
2024-10-20 11:58:22 +0200

    small screen hamburger menu icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,6 +28,42 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      <div className="flex flex-1 items-center justify-end pr-4 w-full">
+        <div className="lg:hidden h-16 w-16 flex items-center">
+          <svg
+            version="1.0"
+            id="katman_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 841.89 595.28"
+            xmlSpace="preserve"
+          >
+            <rect
+              x="164.21"
+              y="132.42"
+              fill="#FFFFFF"
+              width="514.08"
+              height="66.64"
+            />
+            <rect
+              x="164.21"
+              y="395.81"
+              fill="#FFFFFF"
+              width="514.08"
+              height="66.64"
+            />
+            <rect
+              x="164.21"
+              y="264.12"
+              fill="#FFFFFF"
+              width="514.08"
+              height="66.64"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };

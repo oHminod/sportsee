32927725196f0f0a2f5a587b31d284298102a79f
oHminod <yvesm13@gmail.com>
2024-10-27 23:02:26 +0100

    refactorisation

--- src/components/dashboard/userInfoCard/icon.tsx
+++ src/components/dashboard/userInfoCard/icon.tsx
@@ -1,45 +1,50 @@
+import { useEffect, useState } from "react";
+
 /**
- * UserInfoCard component to display user information with an icon.
- * @param {Object} props - The properties for the UserInfoCard component.
- * @param {string} props.label - The label for the user information.
- * @param {string} props.value - The value for the user information.
- * @returns {JSX.Element} The UserInfoCard component.
+ * Icon component that displays an image and background color based on the provided name.
+ * @param {Object} props - The properties object.
+ * @param {string} props.name - The name of the icon to display. Can be one of "Calories", "Protéines", "Glucides", or "Lipides".
+ * @returns {JSX.Element} The rendered Icon component.
  */
 const Icon = ({ name }: { name: string }): JSX.Element => {
-  switch (name) {
-    case "Calories":
-      return (
-        <div className="h-[60px] w-[60px] rouded-[6px] flex items-center justify-center relative">
-          <div className="absolute inset-0 rounded-[6px] bg-red opacity-[6.6%]"></div>
-          <img src="/energy.png" alt="icone calories" />
-        </div>
-      );
-    case "Protéines":
-      return (
-        <div className="h-[60px] w-[60px] rouded-[6px] flex items-center justify-center relative">
-          <div className="absolute inset-0 rounded-[6px] bg-[#4AB8FF] opacity-[10%]"></div>
-          <img src="/chicken.png" alt="icone calories" />
-        </div>
-      );
-    case "Glucides":
-      return (
-        <div className="h-[60px] w-[60px] rouded-[6px] flex items-center justify-center relative">
-          <div className="absolute inset-0 rounded-[6px] bg-[#F9CE23] opacity-[10.17%]"></div>
-          <img src="/apple.png" alt="icone calories" />
-        </div>
-      );
-    case "Lipides":
-      return (
-        <div className="h-[60px] w-[60px] rouded-[6px] flex items-center justify-center relative">
-          <div className="absolute inset-0 rounded-[6px] bg-[#FD5181] opacity-[10%]"></div>
-          <img src="/cheeseburger.png" alt="icone calories" />
-        </div>
-      );
-    default:
-      return (
-        <div className="h-[60px] w-[60px] rouded-[6px] bg-red flex items-center justify-center"></div>
-      );
-  }
+  const [image, setImage] = useState<string>("");
+  const [color, setColor] = useState<string>("");
+  const noImage = image === "";
+
+  useEffect(() => {
+    switch (name) {
+      case "Calories":
+        setImage("/energy.png");
+        setColor("bg-red opacity-[6.6%]");
+        break;
+      case "Protéines":
+        setImage("/chicken.png");
+        setColor("bg-[#4AB8FF] opacity-[10%]");
+        break;
+      case "Glucides":
+        setImage("/apple.png");
+        setColor("bg-[#F9CE23] opacity-[10.17%]");
+        break;
+      case "Lipides":
+        setImage("/cheeseburger.png");
+        setColor("bg-[#FD5181] opacity-[10%]");
+        break;
+      default:
+        break;
+    }
+  }, [name]);
+
+  if (noImage)
+    return (
+      <div className="h-[60px] w-[60px] rouded-[6px] bg-red flex items-center justify-center"></div>
+    );
+
+  return (
+    <div className="h-[60px] w-[60px] rouded-[6px] flex items-center justify-center relative">
+      <div className={`absolute inset-0 rounded-[6px] ${color}`}></div>
+      <img src={image} alt={`icone ${name}`} />
+    </div>
+  );
 };
 
 export default Icon;
